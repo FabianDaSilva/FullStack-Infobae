bd4c3dad07d23d404bc62a2563aa5b6eb5a6abf7
Fabian Da Silva <dasilvafabian.j@gmail.com>
2024-10-27 13:28:28 -0300

    feat: Decoded Jwt para mostrar sus datos

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+
+import { Layout } from '../layouts/Layout';
+import { jwtDecode } from 'jwt-decode';
+
 
 const Profile = () => {
+  const token = localStorage.getItem('token');
+  // console.log(token);
+
+  const decoded = jwtDecode(token);
+  console.log(decoded);
+  // console.log(decoded.given_name)
+  const userEmail = decoded.email;
+  const userName = decoded.given_name;
+
+
   return (
-    <div>Profile</div>
+    <Layout hide={true}>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold mb-4">Hola! {userName ? userName : userEmail}</h1>
+        <p className="text-lg mb-4">Esta página sigue en construcción</p>
+        <img
+          src="https://c.tenor.com/29Ok5pc0ivAAAAAd/tenor.gif"
+          alt="Gatito trabajando"
+          className="rounded-lg shadow-lg w-3/12"
+        />
+      </div>
+    </Layout>
   )
 }
 
